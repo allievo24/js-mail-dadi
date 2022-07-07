8d6896f9533dc3d4579b1071131f79902bf74624
saidmoussadek <saiddus24@gmail.com>
2022-07-07 16:52:28 +0200

    pushe modifica

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 /* creo una lista chiedo un numero al utente;
  cerco se il numero del utente ha una posizione nella lista
  se si stampo un messaggio seno stampo un altro   */
-const numeri = [1, 5, 10, 20];
-const indiceMassimo = numeri.length - 1;
-let number = parseInt(prompt('Inserisci un numero'));
+const email = ['franco@gmail.com', 'franca@gmail.com', 'francesca@gmail.com', 'francesco@gmail.com'];
+ const grandezza_array = email.length ;
+ let mail_utente = parseInt(prompt('Inserisci un email'));
+
+ for(let i = 0; i < grandezza_array; i++) {
+	 
+  	if(email[i] == mail_utente) {
+    	alert("Email trovata"); 
+    }else{
+        alert("Email non trovata"); 
+        
+        console.log(email[i])
+    }
+ }
 
 
 
-if (  number < 0 || number < indiceMassimo) {
+
+
+
+
+ /*if (  number < 0 || number < indiceMassimo) {
     console.log('sei nelle lista');
 } else {
     console.log("non sei nella lista");
 }
- console.log(indiceMassimo)
+ console.log(indiceMassimo)*/1
